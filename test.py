15f8aecea14c53fd1bf654e8f3084479324ffbf0
FCh44 <franckcharrierDev@gmail.com>
2022-10-30 09:54:34 +0100

    fin 1ere partie

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 from decimal import Decimal
+import os
 print((Decimal('0.1')+Decimal('0.2'))==Decimal('0.3'))
+age = 10
+sexe='F'
+user = "admin"
+mdp = "admin"
+if mdp == "admin" or user == "admin":
+    print("Bienvenue !")
+
+liste = [3, 5, 7, 3, 1, 2, 3, 8, 3, 0, 2]
+liste.remove(3,3,3,3)
+print()
+
+
+
+
+
+
+
+
