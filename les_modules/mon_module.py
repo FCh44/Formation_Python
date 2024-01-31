bec778d348228eec81932c968339b342f2598bcd
FCh44 <franckcharrierDev@gmail.com>
2024-01-31 07:44:10 +0100

    modules

--- les_modules/mon_module.py
+++ les_modules/mon_module.py
@@ -1,2 +1,5 @@
 def saluer(name:str):
     print(f"Bonjour {name}")
+
+if __name__=="__main__":
+    saluer("Albert")
---
+++ les_modules/test_modules.py
@@ -0,0 +1,7 @@
+import mon_module
+import sys
+from pprint import pprint
+
+mon_module.saluer("John Doe")
+
+pprint(sys.path)
